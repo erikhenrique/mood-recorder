b478ca036a5cd8ab809fcbb2745e821a32e0b04c
Erik Silva <contato@erikhenrique.com.br>
2022-04-06 11:40:09 -0300

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mood-recorder
+
+This is the mood recorder
+
+## Instalation
+
+
+## Usage
+```
+$ mood good "Code Review time was improved"
+$ mood improve "Focus on technical debts"
+$ mood tks silva "it was very funny our last pair!"
+$ mood clear: Clean the whole history
+$ mood show: Shows all your moods
+```
---
+++ main.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+
+CONFIG_FILE_PREFIX=~/.mood-recorder/history
+CONFIG_FILE_EXT=.txt
+DATE_FORMAT="+%d/%m - %A"
+
+
+help()
+{
+	echo '== This is the mood recorder =='
+	echo 'usage:'
+	echo '$ mood good "\e[3mCode Review time was improved\e[0m"'
+	echo '$ mood improve "\e[3mFocus on technical debts\e[0m"'
+	echo '$ mood tks \e[3msilva\e[0m "\e[3mit was very funny our last pair!\e[0m"'
+	echo '$ mood clear: Clean the whole history'
+	echo '$ mood show: Shows all your moods'
+	
+}
+
+
+recorder()
+{
+	echo "[$1] $2" >> "$CONFIG_FILE_PREFIX-$STATUS$CONFIG_FILE_EXT"
+}
+
+
+tks()
+{
+	echo "[$1] $2: $3" >> "$CONFIG_FILE_PREFIX-tks$CONFIG_FILE_EXT"
+}
+
+
+show()
+{
+	cat "$CONFIG_FILE_PREFIX-good$CONFIG_FILE_EXT"
+	if [ -s "$CONFIG_FILE_PREFIX-good$CONFIG_FILE_EXT" ]; then
+		echo "What was good? "
+		cat "$CONFIG_FILE_PREFIX-good$CONFIG_FILE_EXT"
+		echo ''
+	fi
+	echo "What we have to improve? "
+	cat "$CONFIG_FILE_PREFIX-improve$CONFIG_FILE_EXT"
+	echo ''
+	echo 'Special thanks to:'
+	cat "$CONFIG_FILE_PREFIX-tks$CONFIG_FILE_EXT"
+}
+
+
+clear()
+{
+    echo '' > "$CONFIG_FILE_PREFIX-good$CONFIG_FILE_EXT"
+	echo '' > "$CONFIG_FILE_PREFIX-improve$CONFIG_FILE_EXT" 
+	echo '' > "$CONFIG_FILE_PREFIX-tks$CONFIG_FILE_EXT"
+}
+
+
+mood()
+{
+	STATUS=$1
+	MENSAGEM=$2
+	TIMESTAMP=$(date "$DATE_FORMAT")
+
+	case "$STATUS" in
+		"show")
+			show
+		;;
+		"clear")
+			clear
+		;;
+		"good")
+			recorder $TIMESTAMP $MENSAGEM
+		;;
+		"improve")
+			recorder $TIMESTAMP $MENSAGEM
+		;;
+		"tks")
+			tks $TIMESTAMP $2 $3
+		;;
+		"help")
+			help
+		;;	
+	esac
+
+}
